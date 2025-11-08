0017bd1163541c563dfdfa28eb2702eead36536b
Tamal Mallick <tamalmallick23@gmail.com>
2025-11-08 07:20:36 +0530

    Update Delete_elem.py

--- Delete_elem.py
+++ Delete_elem.py
@@ -4,8 +4,10 @@ marks={"John":89,"Lisa":65,"Peter":90,"Ravi":50}
 del marks["John"]
 print(marks)
 
+
 # Using Pop() function
 
 marks={"John":89,"Lisa":65,"Peter":90,"Ravi":50}
 pop_item=marks.pop("Lisa")
+
 print(marks)
