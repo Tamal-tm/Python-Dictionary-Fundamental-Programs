212763e81324dcac08c0101d3a45e254e8019c7c
Tamal Mallick <tamalmallick23@gmail.com>
2025-11-08 07:16:04 +0530

    Add files via upload

---
+++ Delete_elem.py
@@ -0,0 +1,11 @@
+ # Using delete statement
+
+marks={"John":89,"Lisa":65,"Peter":90,"Ravi":50}
+del marks["John"]
+print(marks)
+
+# Using Pop() function
+
+marks={"John":89,"Lisa":65,"Peter":90,"Ravi":50}
+pop_item=marks.pop("Lisa")
+print(marks)
