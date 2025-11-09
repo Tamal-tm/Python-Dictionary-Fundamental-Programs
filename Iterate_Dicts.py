87b7039f5d0e9d050242c3ba189af480d723b20f
Tamal Mallick <tamalmallick23@gmail.com>
2025-11-09 09:25:47 +0530

    Add files via upload

---
+++ Iterate_Dicts.py
@@ -0,0 +1,22 @@
+friends={"Rachel":"Ross","Monica":"Chandler","Phoebe":'Joe'}
+print(friends)
+
+print(friends["Monica"])
+
+# Using items
+
+for key, value in friends.items():
+    print(key,"-", value)
+
+# Using keys
+
+for key in friends:
+    print(key, ":", friends[key])
+
+# Using keys and values
+
+for key in friends.keys():
+    print(key)
+
+for i in friends.values():
+    print(i)
