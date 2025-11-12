ede743e24d5e2da07253fe156d22ef97567da487
Tamal Mallick <tamalmallick23@gmail.com>
2025-11-12 06:44:38 +0530

    Update Merge_Dicts.py

--- Merge_Dicts.py
+++ Merge_Dicts.py
@@ -10,6 +10,7 @@ print(dict1 | dict2)
 dict1={"John":89,"Lisa":45}
 dict2={"Lisa":94,"Peter":77}
 
+
 print({**dict1,**dict2})
 
 # Using Copy and Update method
@@ -22,6 +23,7 @@ dict3.update(dict1)
 
 
 print(dict3)
+
 
 
 
