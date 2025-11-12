5ff516c322c6809f9ad7ded2c3004af8e41dc7f8
Tamal Mallick <tamalmallick23@gmail.com>
2025-11-12 06:42:42 +0530

    Add files via upload

---
+++ Merge_Dicts.py
@@ -0,0 +1,24 @@
+# Using Bar operator
+
+dict1={"John":89,"Lisa":45}
+dict2={"Lisa":94,"Peter":77}
+
+print(dict1 | dict2) 
+
+
+# Using ** Operator.
+
+dict1={"John":89,"Lisa":45}
+dict2={"Lisa":94,"Peter":77}
+
+print({**dict1,**dict2})
+
+# Using Copy and Update method
+
+dict1={"John":89,"Lisa":45}
+dict2={"Lisa":94,"Peter":77}
+
+dict3=dict2.copy()
+dict3.update(dict1)
+
+print(dict3)
