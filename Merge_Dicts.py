f60b4cf2bbe99b6810367407d76f0b9fc5cf9b99
Tamal Mallick <tamalmallick23@gmail.com>
2025-11-12 06:42:59 +0530

    Update Merge_Dicts.py

--- Merge_Dicts.py
+++ Merge_Dicts.py
@@ -6,6 +6,7 @@ dict2={"Lisa":94,"Peter":77}
 print(dict1 | dict2) 
 
 
+
 # Using ** Operator.
 
 dict1={"John":89,"Lisa":45}
@@ -20,5 +21,6 @@ dict2={"Lisa":94,"Peter":77}
 
 dict3=dict2.copy()
 dict3.update(dict1)
+
 
 print(dict3)
