47431b939bf263d295d072fc7fb4c5c3bfb3b500
Tamal Mallick <tamalmallick23@gmail.com>
2025-11-13 18:45:48 +0530

    Update Nested_Dict.py

--- Nested_Dict.py
+++ Nested_Dict.py
@@ -1,10 +1,10 @@
 # Using 3rd variable
 
-
 from pathlib import Path
 
 Path("C:/Users/catas/Downloads/Dajal Materials/Essentials/new-dict/demo-dict").mkdir(parents=True, exist_ok=True) # Takes two parameters(uses logic)(needed in some scenarios) Will create a new directory
 
 # If we run again, will give error, since same file exists. So to not have any erroe, exists_ok=True. Default value is False.
+
 
 
