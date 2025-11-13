031e3759b6feda15150696bcb1110f0be8bc000f
Tamal Mallick <tamalmallick23@gmail.com>
2025-11-13 18:43:43 +0530

    Add files via upload

---
+++ Nested_Dict.py
@@ -0,0 +1,8 @@
+# Using 3rd variablr
+
+
+from pathlib import Path
+
+Path("C:/Users/catas/Downloads/Dajal Materials/Essentials/new-dict/demo-dict").mkdir(parents=True, exist_ok=True) # Takes two parameters(uses logic)(needed in some scenarios) Will create a new directory
+
+# If we run again, will give error, since same file exists. So to not have any erroe, exists_ok=True. Default value is False.
