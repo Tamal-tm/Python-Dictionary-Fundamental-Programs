5ee1a591aca03c810a0f25d852271691e1a08230
Tamal Mallick <tamalmallick23@gmail.com>
2025-11-14 06:18:59 +0530

    Update Sort_Dict.py

--- Sort_Dict.py
+++ Sort_Dict.py
@@ -1,6 +1,7 @@
 marks={"John":42,'Natalie':89,"Kinland":50} # Key stands at 0, value stands at 1
 print(marks)
 
+
 # Based on values
 
 
@@ -16,4 +17,5 @@ print(v)
 v=sorted(marks.keys())
 
 print(v)
+
 
